b4cc74f422d39526b76117d1e16e0b2a023756c0
Wizarphics <ireoluwaomtosho7@gmail.com>
2023-01-02 08:05:39 +0100

    Updated the email config

--- configs/Email.php
+++ configs/Email.php
@@ -12,14 +12,21 @@ class Email extends ConfigsEmail
     {
         parent::__construct();
 
-        $this->fromEmail = 'wizarframework@gmail.com';
+
+        $this->fromEmail = 'wizarphics@gmail.com';
         $this->SMTPHost = 'smtp.mailtrap.io';
         $this->SMTPPort = 2525;
         $this->SMTPUser = '31cf7c857cf3d7';
         $this->SMTPPass = 'd2cdd77a9f91ea';
         $this->protocol = 'smtp';
         $this->SMTPCrypto = 'tls';
+        $this->SMTPKeepAlive = true;
+        $this->SMTPTimeout = 60;
+        $this->mailType = 'html';
         $this->CRLF = "\r\n";
         $this->newline = "\r\n";
+        $this->charset = "iso-8859-1";
+        $this->DSN = true;
+
     }
 }
